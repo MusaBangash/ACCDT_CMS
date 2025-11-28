fe8d6f135329810d6e188ebd746db142ec461855
MusaBangash <mmkb5055@gmail.com>
2025-11-28 12:54:03 +0500

    add serval files

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
+
 from flask import Flask,render_template
+from flask_scss import Scss
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key_here'
+
+#My App
+
+app= Flask(__name__)
+Scss(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/dashbord")
+def dashboard():
+    return render_template("dashboard.html")
 
 
-@app.route('/')
-def home():
-    return render_template('home.html')
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
